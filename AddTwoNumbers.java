33889b74d4cf5baadae46b508ed4519d68aa52d4
nratnam <nratnam@ufl.edu>
2022-03-14 22:58:30 -0400

    Update AddTwoNumbers.java
    Fixed the addition program.

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class AddTwoNumbers {
-    public static void main (String[] str) {
-        Add numbers
+    public static void main (String[] args) {
+        int number1, number2, sum;
+
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        number1 = scnr.nextInt();
+        System.out.println("Enter second number: ");
+        number2 = scnr.nextInt();
+        sum = number1 + number2;
+
+        System.out.println("Sum : " + sum);
 
     }
 }
