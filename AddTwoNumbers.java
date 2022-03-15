9f74b9bf248724c202aa871ada7037f2f68b6ef5
nratnam <nratnam@ufl.edu>
2022-03-14 23:07:13 -0400

    Update AddTwoNumbers.java
    Added the code for the user to use subtraction.

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -4,12 +4,24 @@ public class AddTwoNumbers {
     public static void main (String[] args) {
         int number1, number2, sum;
 
+        System.out.println("***Addition of two numbers****");
+
         Scanner scnr = new Scanner(System.in);
         System.out.println("Enter first number: ");
         number1 = scnr.nextInt();
         System.out.println("Enter second number: ");
         number2 = scnr.nextInt();
         sum = number1 + number2;
+
+        System.out.println("Sum : " + sum);
+
+        System.out.println("***Subtraction****");
+
+        System.out.println("Enter first number: ");
+        number1 = scnr.nextInt();
+        System.out.println("Enter second number: ");
+        number2 = scnr.nextInt();
+        sum = number1 - number2;
 
         System.out.println("Sum : " + sum);
 
